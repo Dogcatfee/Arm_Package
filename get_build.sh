5b8df8b5c962faa98e52db2679c342a074eff46e
dogcatfee <msessions21@gmail.com>
2017-12-06 20:57:08 -0800

    spelling

--- get_build.sh
+++ get_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone http://github.com/mkfruba/kernel
+git clone http://github.com/mfruba/kernel
 cd kernel/drivers/TL-WN722N_v2.0-Ralink/rtl8188EUS_linux_v4.3.0.8_13968.20150417/
 make ARCH=arm CROSS_COMPILE= -j4
