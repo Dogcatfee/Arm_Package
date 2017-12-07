e3fc93d03dba593f67790ba7558dac49ee1750cf
dogcatfee <msessions21@gmail.com>
2017-12-06 21:28:03 -0800

    update

--- temp_load_modules.sh
+++ temp_load_modules.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rmmod rtl8188eu
+rmmod r8188eu
+rmmod 8188eu
+#rmmod lib80211
+#rmmod cfg80211
 modprobe lib80211
 modprobe cfg80211
-cd kernel/drivers/TL-WN722N_v2.0-Ralink/rtl8188EUS_linux_v4.3.0.8_13968.20150417/
+cd ./kernel/drivers/TL-WN722N_v2.0-Ralink/rtl8188EUS_linux_v4.3.0.8_13968.20150417/
 insmod ./8188eu.ko
 
